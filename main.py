53fde1e8391ffa534544fef9a37b44cd580b927d
DungCDA <dung.cda@teko.vn>
2024-01-19 17:08:27 +0700

    update

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ def generate_invoice_content(ledgers):
     for ledger in ledgers:
         code = ledger['code']
         name = ledger['name']
-        res = res + f'<p style="padding: 10px; font-weight: bold;">{code} - {name}</p>' + '\n'
+        res = res + f'''
+        <tr>
+            <td colspan="6" style="font-weight: bold">
+                {code} - {name} 
+            </td>
+        </tr>''' + '\n'
         allocations = ledger['cost_allocations']
         allocations_table = generate_allocations_table(allocations)
         res = res + allocations_table + '\n'
@@ -33,11 +38,7 @@ def generate_allocations_table(allocations):
             <td align="right">&euro;{allocation['total']}</td>
         </tr>
         '''
-    return f'''
-    <table style="margin-top: 5%; border: 0;" width="100%">
-    {res}
-    </table>
-    '''
+    return res
 
 
 
@@ -66,54 +67,56 @@ def generate_invoice():
     content = generate_invoice_content(ledgers)
 
     html_content = f'''
-    <html>
-    <head>
-    </head>
-    <body style="margin-left: 20%; margin-right: 20%; font-family: 'Helvetica', sans-serif;">
-    <div style="display: flex;">
-        <div style="width: 33.33%" align="left">
-            <h3>{building_name}</h3>
-            <p>{company_number}</p>
-            <p>{address_line_1}</p>
-            <p>{address_line_2}</p>
-        </div>
-        <div style="width: 33.33%" align="center">
-            <h3>Facturenlijst - grootboekrekening</h3>
-            <p>{date_start} - {date_end}</p>
-        </div>
-        <div style="width: 33.33%" align="right">
-            <h3>{export_date}</h3>
-            <p>Pagina {current_page} van {total_page}</p>
+    <div style="margin: auto; width: 595px;">
+        <div style="display: flex; flex-direction: column; height: 100%; width: 100%; margin: 24px; line-height: 16px; word-wrap: break-word; font-size: 11px; font-family: Inter; justify-content: space-between;">
+            <div>
+                <div style="display: flex">
+                    <div style="width: 33%" align="left">
+                        <p style="font-weight: bold;">{building_name}</p>
+                        <p>{company_number}</p>
+                        <p>{address_line_1}</p>
+                        <p>{address_line_2}</p>
+                    </div>
+                    <div style="width: 33%" align="center">
+                        <p style="font-weight: bold;">Facturenlijst - grootboekrekening</p>
+                        <p>{date_start} - {date_end}</p>
+                    </div>
+                    <div style="width: 33%" align="right">
+                        <p style="font-weight: bold;">{export_date}</p>
+                        <p>Pagina {current_page} van {total_page}</p>
+                    </div>
+                </div>
+                <div>
+                    <table style="width: 100%; margin-top: 16px; border: 0; border-spacing: 0px 8px; line-height: 16px; word-wrap: break-word; font-size: 11px; font-family: Inter;">
+                        <!--column header-->
+                        <tr>
+                            <td style="border-bottom: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left">Datum</td>
+                            <td style="border-bottom: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left">Factuurnr.</td>
+                            <td style="border-bottom: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left">Leverancier.</td>
+                            <td style="border-bottom: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left">Omschrijving</td>
+                            <td style="border-bottom: 0.5px solid grey; width: 72px; padding-right: 8px; font-weight: bold;" align="right">BTW</td>
+                            <td style="border-bottom: 0.5px solid grey; width: 72px; padding-left: 4px; font-weight: bold;" align="right">Totaal</td>
+                        </tr>
+                        {content}
+                    </table>
+                </div>
+            </div>
+    
+            <div>
+                <table style="width: 100%; margin-top: 16px; border: 0; border-spacing: 0px 8px; line-height: 16px; word-wrap: break-word; font-size: 11px; font-family: Inter;">
+                    <!--footer-->
+                    <tr>
+                        <td style="border-top: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left">Totaal</td>
+                        <td style="border-top: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left"></td>
+                        <td style="border-top: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left"></td>
+                        <td style="border-top: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left"></td>
+                        <td style="border-top: 0.5px solid grey; width: 72px; padding-right: 8px; font-weight: bold;" align="right">&euro;{sum_vat_amount}</td>
+                        <td style="border-top: 0.5px solid grey; width: 72px; padding-left: 4px; font-weight: bold;" align="right">&euro;{sum_total_amount}</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     </div>
-    
-    <!--column header-->
-    <table style="margin-top: 5%; border: 0;" width="100%">
-        <tr>
-            <td style="padding: 10px; font-weight: bold;" align="left">Datum</td>
-            <td style="padding: 10px; font-weight: bold;" align="left">Factuurnr.</td>
-            <td style="padding: 10px; font-weight: bold;" align="left">Leverancier.</td>
-            <td style="padding: 10px; font-weight: bold;" align="left">Omschrijving</td>
-            <td style="padding: 10px; font-weight: bold;" align="right">BTW</td>
-            <td style="padding: 10px; font-weight: bold;" align="right">Totaal</td>
-        </tr>
-    </table>
-    <hr>
-    
-    {content}
-    
-    <!--footer-->
-    <hr>
-    <table style="margin-top: 5%; border: 0;" width="100%">
-        <tr>
-            <td style="font-weight: bold;" align="left">Totaal</td>
-            <td style="width: 100%"></td>
-            <td style="font-weight: bold;" align="right">&euro;{sum_vat_amount}</td>
-            <td style="font-weight: bold;" align="right">&euro;{sum_total_amount}</td>
-        </tr>
-    </table>
-    </body>
-    </html>
     '''
 
     pdf_bytes = pdfkit.from_string(html_content, False, configuration=pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf))
