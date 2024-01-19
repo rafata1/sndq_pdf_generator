8aba929e30615b0b4fbb787ed3bbcf49b9185efd
DungCDA <dung.cda@teko.vn>
2024-01-19 21:07:03 +0700

    update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 path_wkhtmltopdf = "/usr/bin/wkhtmltopdf"
 
+
 def generate_invoice_content(ledgers):
     res = ''
     for ledger in ledgers:
@@ -41,8 +42,6 @@ def generate_allocations_table(allocations):
     return res
 
 
-
-
 @app.route('/generate_invoice', methods=['POST'])
 def generate_invoice():
     # Retrieve data from the request
@@ -58,9 +57,6 @@ def generate_invoice():
     date_end = building['date_end']
     sum_total_amount = building['sum_total_amount']
     sum_vat_amount = building['sum_vat_amount']
-    pagination = building['pagination']
-    current_page = pagination['current_page']
-    total_page = pagination['total_page']
     export_date = building['export_date']
 
     ledgers = building['ledgers']
@@ -70,7 +66,7 @@ def generate_invoice():
     <div style="margin: auto; width: 595px;">
         <div style="display: flex; flex-direction: column; height: 100%; width: 100%; margin: 24px; line-height: 16px; word-wrap: break-word; font-size: 11px; font-family: Inter; justify-content: space-between;">
             <div>
-                <div style="display: flex">
+                <div style="display: -webkit-box; display: -webkit-flex;">
                     <div style="width: 33%" align="left">
                         <p style="font-weight: bold;">{building_name}</p>
                         <p>{company_number}</p>
@@ -83,35 +79,33 @@ def generate_invoice():
                     </div>
                     <div style="width: 33%" align="right">
                         <p style="font-weight: bold;">{export_date}</p>
-                        <p>Pagina {current_page} van {total_page}</p>
                     </div>
                 </div>
                 <div>
-                    <table style="width: 100%; margin-top: 16px; border: 0; border-spacing: 0px 8px; line-height: 16px; word-wrap: break-word; font-size: 11px; font-family: Inter;">
+                    <table style="width: 98%; margin-top: 16px; border: 0; border-spacing: 0px 8px; line-height: 16px; word-wrap: break-word; font-size: 11px; font-family: Inter;">
                         <!--column header-->
                         <tr>
-                            <td style="border-bottom: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left">Datum</td>
-                            <td style="border-bottom: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left">Factuurnr.</td>
-                            <td style="border-bottom: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left">Leverancier.</td>
-                            <td style="border-bottom: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left">Omschrijving</td>
-                            <td style="border-bottom: 0.5px solid grey; width: 72px; padding-right: 8px; font-weight: bold;" align="right">BTW</td>
-                            <td style="border-bottom: 0.5px solid grey; width: 72px; padding-left: 4px; font-weight: bold;" align="right">Totaal</td>
+                            <td style="border-bottom: 1px solid grey; padding-right: 8px; font-weight: bold;" align="left">Datum</td>
+                            <td style="border-bottom: 1px solid grey; padding-right: 8px; font-weight: bold;" align="left">Factuurnr.</td>
+                            <td style="border-bottom: 1px solid grey; padding-right: 8px; font-weight: bold;" align="left">Leverancier.</td>
+                            <td style="border-bottom: 1px solid grey; padding-right: 8px; font-weight: bold;" align="left">Omschrijving</td>
+                            <td style="border-bottom: 1px solid grey; width: 72px; padding-right: 8px; font-weight: bold;" align="right">BTW</td>
+                            <td style="border-bottom: 1px solid grey; width: 72px; padding-left: 4px; font-weight: bold;" align="right">Totaal</td>
                         </tr>
                         {content}
                     </table>
                 </div>
             </div>
-    
             <div>
-                <table style="width: 100%; margin-top: 16px; border: 0; border-spacing: 0px 8px; line-height: 16px; word-wrap: break-word; font-size: 11px; font-family: Inter;">
+                <table style="width: 98%; margin-top: 16px; border: 0; border-spacing: 0px 8px; line-height: 16px; word-wrap: break-word; font-size: 11px; font-family: Inter;">
                     <!--footer-->
                     <tr>
-                        <td style="border-top: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left">Totaal</td>
-                        <td style="border-top: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left"></td>
-                        <td style="border-top: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left"></td>
-                        <td style="border-top: 0.5px solid grey; padding-right: 8px; font-weight: bold;" align="left"></td>
-                        <td style="border-top: 0.5px solid grey; width: 72px; padding-right: 8px; font-weight: bold;" align="right">&euro;{sum_vat_amount}</td>
-                        <td style="border-top: 0.5px solid grey; width: 72px; padding-left: 4px; font-weight: bold;" align="right">&euro;{sum_total_amount}</td>
+                        <td style="border-top: 1px solid grey; padding-right: 8px; font-weight: bold;" align="left">Totaal</td>
+                        <td style="border-top: 1px solid grey; padding-right: 8px; font-weight: bold;" align="left"></td>
+                        <td style="border-top: 1px solid grey; padding-right: 8px; font-weight: bold;" align="left"></td>
+                        <td style="border-top: 1px solid grey; padding-right: 8px; font-weight: bold;" align="left"></td>
+                        <td style="border-top: 1px solid grey; width: 72px; padding-right: 8px; font-weight: bold;" align="right">&euro;{sum_vat_amount}</td>
+                        <td style="border-top: 1px solid grey; width: 72px; padding-left: 4px; font-weight: bold;" align="right">&euro;{sum_total_amount}</td>
                     </tr>
                 </table>
             </div>
@@ -119,7 +113,17 @@ def generate_invoice():
     </div>
     '''
 
-    pdf_bytes = pdfkit.from_string(html_content, False, configuration=pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf))
+    options = {
+        'page-size': 'A5',
+        'footer-right': 'Pagina [page] van [topage]',
+    }
+
+    pdf_bytes = pdfkit.from_string(
+        html_content,
+        False,
+        configuration=pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf),
+        options=options,
+    )
     pdf_io = BytesIO(pdf_bytes)
     return send_file(pdf_io, download_name="invoice.pdf", as_attachment=True)
 
